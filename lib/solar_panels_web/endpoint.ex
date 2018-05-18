01122da46ad60a93475994bb80922186717a414d
Geovane Fedrecheski <geonnave@gmail.com>
2018-05-18 17:53:03 -0300

    Fix socket /solar route

--- lib/solar_panels_web/endpoint.ex
+++ lib/solar_panels_web/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule SolarPanelsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :solar_panels
 
-  socket "/socket", SolarPanelsWeb.UserSocket
+  socket "/solar/socket", SolarPanelsWeb.UserSocket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
