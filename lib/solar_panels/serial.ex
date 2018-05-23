76494d3daac22581732ba3e6ad29207c98bb2d80
Geovane Fedrecheski <geonnave@gmail.com>
2018-05-23 08:59:20 -0300

    Save panel data to csv file

--- lib/solar_panels/serial.ex
+++ lib/solar_panels/serial.ex
@@ -34,11 +34,13 @@ defmodule SolarPanels.Serial do
     Logger.debug "will broadcast #{data}"
     case Poison.decode(data) do
       {:ok, data} ->
+        data = put_in data, ["timestamp"], SolarPanels.now_unix()
         payload = %{
-          "current" => %{"value" => data["current"], "timestamp" => SolarPanels.now_unix()},
-          "voltage" => %{"value" => data["voltage"], "timestamp" => SolarPanels.now_unix()}
+          "current" => %{"value" => data["current"], "timestamp" => data["timestamp"]},
+          "voltage" => %{"value" => data["voltage"], "timestamp" => data["timestamp"]}
         }
         SolarPanelsWeb.Endpoint.broadcast! "panels:real", "value", payload
+        SolarPanels.Storage.save_to_file(data)
 
       {:error, reason} ->
         Logger.warn "Could not parse data because of reason #{inspect reason}"
--- lib/solar_panels/storage.ex
+++ lib/solar_panels/storage.ex
@@ -1,5 +1,14 @@
 defmodule SolarPanels.Storage do
   def save_to_file(data) do
-    File.write!("filename", data)
+    filename = Path.join(:code.priv_dir(:solar_panels), "readings_#{Date.utc_today |> Date.to_string}.csv")
+    File.open(filename, [:append], fn file ->
+      IO.write(file, serialize(data))
+    end)
+  end
+
+  def serialize(%{"timestamp" => ts, "current" => c, "voltage" => v}) do
+    c = :erlang.float_to_binary c, decimals: 16
+    v = :erlang.float_to_binary v, decimals: 16
+    "#{ts},#{c},#{v}\n"
   end
 end
