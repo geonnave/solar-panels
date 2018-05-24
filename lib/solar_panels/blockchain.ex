5fd3dbd6080459cbc5ca01d4474fb450e115ec03
Geovane Fedrecheski <geonnave@gmail.com>
2018-05-24 12:24:15 -0300

    Adjust to save on blockchain

--- lib/solar_panels/blockchain.ex
+++ lib/solar_panels/blockchain.ex
@@ -1,26 +1,14 @@
 defmodule SolarPanels.Blockchain do
-  use GenServer
   require Logger
 
-  def start_link do
-    GenServer.start_link __MODULE__, [], name: __MODULE__
-  end
-
   def add_transaction(data) do
-    GenServer.cast __MODULE__, {:add_transaction, data}
-  end
-
-  def init(_) do
-    socket_opts = [:binary, active: false]
-    {:ok, socket} = :gen_tcp.connect('127.0.0.1', 1735, socket_opts)
-    Logger.info "Connected to socket #{inspect socket}"
-    {:ok, socket}
-  end
-
-  def handle_cast({:add_transaction, data}, socket) do
     Logger.debug "Transaction data is #{data}"
-    IO.inspect :gen_tcp.send(socket, data)
-    IO.inspect :gen_tcp.close(socket)
-    {:noreply, socket}
+    case :gen_tcp.connect('127.0.0.1', 1735, [:binary, active: false]) do
+      {:ok, socket} ->
+        :gen_tcp.send(socket, data)
+        :gen_tcp.close(socket)
+      {:error, reason} ->
+        Logger.warn "Could not connect to 127.0.0.1:1735 (#{inspect reason})"
+    end
   end
 end
